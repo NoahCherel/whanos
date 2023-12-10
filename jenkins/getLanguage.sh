1c05d1c31016b646102aabbf2907900179db9e07
Noah <noah.cherel@epitech.eu>
2023-12-10 18:34:31 +0100

    fzsfv

--- jenkins/getLanguage.sh
+++ jenkins/getLanguage.sh
@@ -1,28 +1,35 @@
 #!/bin/bash
 
-if [ $1 == "" ]; then
-    # >&2 echo "./getLanguage.py [folder]\n\tfolder: folder path (default: current folder)"
-    echo "WHY"
+if [ -z "$1" ]; then
+    echo "Usage: $0 [folder]"
+    echo "folder: folder path (default: current folder)"
     exit 1
 fi
 
-EXTENSIONS=("c:Makefile" "java:app/pom.xml" "javascript:package.json" "python:requirements.txt" "befunge:app/main.bf")
+declare -A EXTENSIONS=(
+    ["c"]="Makefile"
+    ["java"]="app/pom.xml"
+    ["javascript"]="package.json"
+    ["python"]="requirements.txt"
+    ["befunge"]="app/main.bf"
+)
+
+folder_path="$1"
 FOUND=()
 
-for ext in "${EXTENSIONS[@]}"; do
-    KEY=${ext%%:*}
-    VALUE=${ext#*:}
+for key in "${!EXTENSIONS[@]}"; do
+    file_path="$folder_path/${EXTENSIONS[$key]}"
 
-    if test -f $1/$VALUE; then
-        FOUND+=($KEY)
+    if test -f "$file_path"; then
+        FOUND+=("$key")
     fi
 done
 
-if [[ ${#FOUND[@]} -eq 0 ]]; then
-    >&2 echo "No valid extension found"
+if [ ${#FOUND[@]} -eq 0 ]; then
+    >&2 echo "Not found"
     exit 1
-elif [[ ${#FOUND[@]} -gt 1 ]]; then
-    >&2 echo "Multiple extensions found"
+elif [ ${#FOUND[@]} -gt 1 ]; then
+    >&2 echo "found"
     exit 1
 else
     echo "${FOUND[0]}"
